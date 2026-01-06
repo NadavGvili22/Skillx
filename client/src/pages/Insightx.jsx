c1c610d04f69dfacbe851498b3d25822f96fae72
Michael Volobuev <michaelv000king@gmail.com>
2026-01-06 11:18:51 +0200

    added SkillTrackX to home hab

--- client/src/pages/Insightx.jsx
+++ client/src/pages/Insightx.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Insightx() {
@@ -16,3 +15,4 @@ export default function Insightx() {
     </section>
   )
 }
+;
